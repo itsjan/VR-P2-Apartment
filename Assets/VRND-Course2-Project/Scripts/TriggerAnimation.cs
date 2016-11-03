5cf656f1b3d16c19ead07556f330b6ee053b811c
Jan K <jan.kaiponen@gmail.com>
2016-11-03 18:02:01 +0200

    Initial commit

---
+++ Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAnimation : MonoBehaviour {
+    public string AnimationName;
+    public Animator stateMachine;
+
+    private bool created = false;
+
+	// Reference to the Google VR object in the scene
+	private GvrViewer viewer;
+
+
+    void Awake() {
+        if (GvrViewer.Instance == null) {
+            GvrViewer.Create();
+            created = true;
+        }
+		// Locate the GvrViewer instance
+		viewer = (GvrViewer)FindObjectOfType(typeof(GvrViewer));
+		if (viewer == null) {
+			Debug.LogError("No GvrViewer found. Please drag the GvrViewerMain prefab into the scene.");
+			return;
+		}
+
+    }
+
+    void Start() {
+        if (created) {
+            foreach (Camera c in GvrViewer.Instance.GetComponentsInChildren<Camera>()) {
+                c.enabled = false; // to use the Gvr SDK without adding cameras we have to disable them
+            }
+        }
+
+    }
+
+    void Update() {
+        GvrViewer.Instance.UpdateState(); //need to update the data here otherwise we dont get mouse clicks; this is because we are automatically creating the GVRSDK (seems like a bug)
+		// Don't do anything unless the Google VR object and camera can be located
+		viewer = (GvrViewer)FindObjectOfType(typeof(GvrViewer));
+		if (viewer == null) {
+			return;
+		}
+		if (viewer.Triggered) {
+			stateMachine.SetTrigger (AnimationName);
+
+		}
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# "Build an Apartment" Project
+
+By Jan Kaiponen
+
+Learning objectives:
+* Adding 3D models to a scene
+* Moving, scaling, and rotating 3D models
+* Deploying to your Cardboard Viewer
+* Creating materials and assigning textures
+* Creating animations and triggering them
+* Creating lights and baking them
+* Optimizing a scene for top performance
+
+## Screenshots
+
+Screenshot is included in folder Screenshots
+
+## Models
+
+* Apartment is furnished with provided assets.
+* All models are marked Static, exept the animated globe.
+
+## Google VR
+
+* Google Cardboard support is added. Running the scene, the Cardboad stereoscopic view appears automatically.
+
+## Lightning
+
+* All lights are marked as Static.
+* All lights are baked and none are realtime
+* Baked resolution is 80, with uncompressed lightmap
+
+## Globe
+
+* Animation is implemented as in the Project Rubric. When you press the Cardboard button, the Globe starts to spin. The globe will spin until the button is pressed again.
+
+
+## Build
+
+Android apk file is included in folder Build
